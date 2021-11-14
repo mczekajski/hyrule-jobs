5d62e3aecfc75e4d6a213836e88f65883401d2da
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-11-14 18:41:11 +0100

    basic data, method and template

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <p>{{ name }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      name: 'Link'
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
