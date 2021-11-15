2e99f2751d7d2c60418f6dd663aad6ef8b11b8fc
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-11-15 21:52:34 +0100

    refactor code with refs

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {},
-  data() {
-    return {
-      name: 'Link',
-      age: 25 as number | string
-    }
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
+
+    // return { ...toRefs(state) }
+
+    const name = ref('Link')
+    const age = ref<number | string>(25)
+
+    return { name, age }
   },
   methods: {
     changeName(name: string) {
@@ -31,5 +38,4 @@ export default defineComponent({
 </script>
 
 <style>
-
 </style>
